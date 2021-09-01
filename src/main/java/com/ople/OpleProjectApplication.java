351be25f822175a3ffb58d751e1fd0aa389c05f7
u-hyun <dbgus9694@gmail.com>
2021-09-01 15:47:04 +0900

    init

---
+++ src/main/java/com/ople/OpleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ople/OpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
