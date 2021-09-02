c34f69edc582c795a2818a3ce6545433793cebbb
u-hyun <dbgus9694@gmail.com>
2021-09-02 20:03:42 +0900

    9/2

--- src/main/java/com/ople/controller/MemberController.java
+++ src/main/java/com/ople/controller/MemberController.java
@@ -1,12 +1,17 @@
 package com.ople.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.ople.domain.Member;
 import com.ople.service.MemberService;
@@ -16,6 +21,32 @@ public class MemberController {
 	
 	@Autowired
 	private MemberService memberService;
+	
+	@ModelAttribute("member")
+	public Member getMember() {
+		return new Member();
+	}
+	
+	@GetMapping("/joinView")
+	public String View() {
+		return "member/joinView";
+	}
+	
+	@RequestMapping("/check_id")
+	@ResponseBody
+	public String check_id(String id) {
+	
+		Optional<Member> member = memberService.findMember(id);
+		Member mem =  member.orElse(new Member());
+		return mem.getMemberId();//값이 없으면(null) ""로 전송된다.
+	}
+	
+	@PostMapping("/join")
+	public String join(Member member, Model m) {
+		Member mem = memberService.saveMember(member);
+		
+		return "redirect:loginform";
+	}  
 	
 	@GetMapping("/loginform")
 	public String loginView(@ModelAttribute("member")Member member) {
@@ -30,11 +61,23 @@ public class MemberController {
 		if(findMember != null && findMember.getMemberPw().equals(member.getMemberPw())) {
 			model.addAttribute("member", findMember);
 			//getBoardList대신 메인페이지 경로로 바꿔줄 것
-			return "redirect:getBoardList";
+			return "redirect:mainPage";
 		}else {
 			return "redirect:loginform";
 		}
 	}
 
+	@GetMapping("/logout")
+	public String logout(SessionStatus status) {
+		status.setComplete();// @SessionAttributes를 활용해 Session에 남긴 데이터를 정리
+		//index.html 대신에 메인? 넣을것
+		return "redirect:index.html";		
+	}
+	
+	
+	
+	
+	
+	
 	
 }
--- src/main/webapp/WEB-INF/ople/member/loginView.jsp
+++ src/main/webapp/WEB-INF/ople/member/loginView.jsp
@@ -5,11 +5,22 @@
 <title>Ople : 로그인</title>
 </head>
 <body>
-<h1>Ople</h1>
+<h1>로그인하고 당신만의 리스트를 확인하세요!</h1>
 <form action="login" method="post">
 <table align="center" cellpadding="0" cellspacint="0">
 <tr>
 	<td><input name="id" type="text" size="10" placeholder="아이디"></td>
+</tr>
+<tr>
+	<td><input name="password" type="password" size="10" placeholder="비밀번호"></td>
+</tr>
+<tr>
+	<td colspan="2" align="center"><input type="submit" value="로그인"></td>  
+</tr>
+<tr>
+	<td>
+		<a href="/findAccountView">아이디/비밀번호찾기</a><a href="/joinView">회원 가입</a>
+	</td>
 </tr>
 </table>
 </form>
